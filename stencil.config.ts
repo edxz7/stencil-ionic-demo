55d5a15a1d97bf7247c097ffb23a861c89530ccd
EChColorado <eduardo.chavez@kavak.com>
2020-09-10 15:06:44 -0500

    feat(library) add custom button

---
+++ stencil.config.ts
@@ -0,0 +1,11 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+export const config: Config = {
+  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/app.css',
+  taskQueue: 'async',
+  outputTargets: [{
+    type: 'www',
+    serviceWorker: null
+  }],
+};
